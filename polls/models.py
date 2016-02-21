93b6fe63284b6b549e87a830da845d42f80adf5f
Yungon Park <hahafree12@gmail.com>
2016-02-21 08:23:54 +0000

    Change class representation by overriding __str__ method

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+@python_2_unicode_compatible    # only if you need to support python 2
 class Question(models.Model):
+    # Override __str__ method.
+    def __str__(self):
+        return self.question_text
+    
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+@python_2_unicode_compatible    # only if you need to support python 2
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+    
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
