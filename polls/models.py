36890461ca1736410c5ce3d0ba5ca846c9d0760a
Yungon Park <hahafree12@gmail.com>
2016-02-21 08:30:06 +0000

    Add custom method.

--- polls/models.py
+++ polls/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 # Create your models here.
 @python_2_unicode_compatible    # only if you need to support python 2
@@ -9,6 +10,10 @@ class Question(models.Model):
     # Override __str__ method.
     def __str__(self):
         return self.question_text
+    
+    # Add custom method.
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
