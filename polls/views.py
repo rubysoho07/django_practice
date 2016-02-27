8e1a1571179d106081bf0b053609a49d0ae5b351
Yungon Park <hahafree12@gmail.com>
2016-02-28 00:46:43 +0900

    Use get_object_or_404 (): if the object doesn't exist, raise Http404.
    import render: related to use render function

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from .models import Question
 
@@ -14,10 +15,7 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	try:
-		question = Question.objects.get(pk=question_id)
-	except Question.DoesNotExist:
-		raise Http404("Question does not exist")
+	question = get_object_or_404 (Question, pk=question_id)
 	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
