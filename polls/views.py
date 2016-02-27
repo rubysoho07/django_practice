39a1ce360a09e2959cdd37fd1226775030e3759b
Yungon Park <hahafree12@gmail.com>
2016-02-28 00:32:00 +0900

    Use render() function. It fills context and returns HttpRequest object.

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ def index(request):
 	context = {
 		'latest_question_list': latest_question_list,
 	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
