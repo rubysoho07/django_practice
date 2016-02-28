bb8eee199438e933bf99efe9ffd148b6ad9d2019
Yungon Park <hahafree12@gmail.com>
2016-02-28 09:34:18 +0900

    Implement vote() function. Learn to deal with POST data and to use HttpResponseRedirect

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from .models import Question
+from .models import Choice, Question
+from django.core.urlresolvers import reverse
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -23,4 +24,19 @@ def results(request, question_id):
 	return HttpResponse(response % question_id)
 
 def vote (request, question_id):
-	return HttpResponse("You're voting on question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+				'question': question,
+				'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully 
+		# dealing with POST data. This prevents data from being posted twice
+		# if a user hits the back button.
+		return HttpResponseRedirect('polls:results', args=(question.id,))
